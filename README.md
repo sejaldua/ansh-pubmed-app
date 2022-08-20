e75e1bc4e1961b06f2cdcf89f11cb12e1c811636
Sejal Dua <sejal.dua@tufts.edu>
2022-08-20 15:01:18 -0700

    coding session

---
+++ README.md
@@ -0,0 +1,10 @@
+# Ansh Pubmed Nanotechnology Research App
+
+## Helpful Commands
+
+- `pip install streamlit`
+- `pip install`
+    - `pip install biopython`
+    - `pip install pymed`
+- `streamlit hello` (to test if the install worked)
+- `streamlit run app.py`
--- app.py
+++ app.py
@@ -14,8 +14,9 @@ def querySearch(keywords):
   pubmed = PubMed(tool="MyTool", email="sejaldua@gmail.com")
   results = pubmed.query(refined_search , max_results=5)
   result_list = list(results)
+  output_str = ""
   if len(result_list) == 0:
-    print("Try Again")
+    return "Try Again"
   else:
    for i, article in enumerate(result_list):
     
@@ -30,14 +31,24 @@ def querySearch(keywords):
           id_index = id.index("\n")
           dicts.update({"pubmed_id" : article.pubmed_id[0:id_index]})
       dicts["link"] = "https://pubmed.ncbi.nlm.nih.gov/" + dicts.get("pubmed_id")
-      print(
-        f'Title: {dicts.get("title")} | Publication Date: {dicts.get("publication_date")} | | PubMed Id: {dicts.get("pubmed_id")}\nJournal: {dicts.get("journal")}\nAuthors: {dicts.get("authors")}\nKeywords: "{dicts.get("keywords")}"\nAbstract: {dicts.get("abstract")}\nLink: {dicts.get("link")}\n'
-      )
-
+      output_str += f'Title: {dicts.get("title")}\n'
+      output_str += f'Publication Date: {dicts.get("publication_date")}\n'
+      output_str += f'PubMed Id: {dicts.get("pubmed_id")}\nJournal: {dicts.get("journal")}\nAuthors: {dicts.get("authors")}\nKeywords: "{dicts.get("keywords")}"\nAbstract: {dicts.get("abstract")}\nLink: {dicts.get("link")}\n'
+  return output_str
 
 diseases = ['Fabry Disease', 'Cystic Fibrosis', 'Hemophilia', 'Brugada Syndrome', 'Scleroderma', 'Primary biliary cholangitis', 'Alzheimer Disease', 'ALS ', 'Muscular dystrophy', 'Spinal Muscular Atrophy']
 treatments = ['Nanoparticle drug delivery systems', 'Nanovaccines', 'nanoparticle-based treatments', 'Nanoparticle drugs', 'Nanoparticles for diagnosis', 'synthetic nanoparticles components']
-st.write(querySearch(diseases[0]))
+
+mode = st.sidebar.selectbox('Select a research focus', ['Rare Diseases', 'Nanotechnology Treatments'])
+if mode == 'Rare Diseases':
+  choices = diseases
+else:
+  choices = treatments
+
+search_term = st.sidebar.selectbox('Select a PubMed query term', choices)
+
+run_query = st.sidebar.button('Learn More!')
+st.text(querySearch(search_term))
 #for i in diseases: 
 #  querySearch(i)
 #for j in treatments:
